fcf944fa2fcf00edc791afe9daec2309fda4cbe5
Sainath-Nanaware <sainathnanaware2001@gmail.com>
2026-06-29 19:24:56 +0530

    add schema validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+//controllers
 const {registration}=require("../controllers/userController")
 
-router.post("/register", registration);
+//validation middleware
+const validate=require("../middlewares/schemaValitation")
+
+//schema validation
+const {registerSchema}=require("../validation/userValidation")
+
+router.post("/register",validate(registerSchema),registration);
 
 module.exports = router;
---
+++ validation/userValidation.js
@@ -0,0 +1,33 @@
+const Joi=require("joi")
+
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(50).required().messages({
+    "string.empty": "Username is required",
+    "string.min": "Username must be at least 3 characters",
+    "string.max": "Username must not exceed 50 characters",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.empty": "Email is required",
+    "string.email": "Email must be valid",
+  }),
+
+  password: Joi.string().min(8).max(8).required().messages({
+    "string.empty": "Password is required",
+    "string.min": "Password must be at least 8 characters",
+  }),
+
+  //consider only indian mobile number +91 is optional
+  mobile: Joi.string()
+    .pattern(/^(\+91)?[6-9]\d{9}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Mobile number must be 10 digits and start with 6-9",
+      "string.empty": "Mobile number cannot be empty",
+      "any.required": "Mobile number is required",
+    }),
+});
+
+
+module.exports={registerSchema}
